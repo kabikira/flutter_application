b1ea442bc7e4c5b3ec2dd929b285636b713145c2
kabikira <kabikirar2@protonmail.com>
2024-12-14 01:20:02 +0900

    よく使うUI

--- lib/main.dart
+++ lib/main.dart
@@ -67,13 +67,148 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
 
-    final body = SafeArea(
-        child: Column(
+// テキストフィールド
+    final textfieldText = Text('メッセージ:$_msg');
+    final controller = TextEditingController();
+
+    final textfield = TextField(
+      controller: controller,
+      autocorrect: true,
+      decoration: const InputDecoration(
+        labelText: "内容",
+        hintText: "100文字以内で入力してください",
+      ),
+      onChanged: (text) {
+        print("value: $text");
+      },
+    );
+
+    setMsg() {
+      setState(() {
+        _msg = controller.text;
+      });
+    }
+
+    clear() {
+      controller.clear();
+    }
+
+    final setMsgButton = ElevatedButton(
+      onPressed: setMsg,
+      child: const Text("反映"),
+    );
+
+    final clearButton = ElevatedButton(
+      onPressed: clear,
+      child: const Text("クリア"),
+    );
+
+    final textfieldArea = Column(
       children: [
-        textArea,
-        buttonArea,
+        textfieldText,
+        textfield,
+        Row(
+          children: [
+            setMsgButton,
+            clearButton,
+          ],
+        ),
       ],
-    ));
+    );
+
+    final isCheckedText = Text("チェック：${_isChecked ? "オン" : "オフ"}");
+
+    final checkbox = Checkbox(
+      value: _isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          _isChecked = value!;
+        });
+      },
+    );
+
+    const checkboxText = Text("チェックボックス");
+
+    final checkboxArea = Row(
+      children: [
+        isCheckedText,
+        checkbox,
+        checkboxText,
+      ],
+    );
+
+    // ラジオボタン
+    final radio1 = Radio(
+      value: 1,
+      groupValue: _radioValue,
+      onChanged: (value) {
+        setState(() {
+          _radioValue = value!;
+        });
+      },
+    );
+    const radiotext1 = Text('Android');
+
+    final radio2 = Radio(
+      value: 2,
+      groupValue: _radioValue,
+      onChanged: (value) {
+        setState(() {
+          _radioValue = value!;
+        });
+      },
+    );
+    const radiotext2 = Text('iOS');
+
+    const radioMap = {
+      0: "未選択",
+      1: "Android",
+      2: "iOS",
+    };
+    final radioText = Text("デバイス：${radioMap[_radioValue]}");
+
+    final radioArea = Row(
+      children: [
+        radio1,
+        radiotext1,
+        const SizedBox(width: 10.0),
+        radio2,
+        radiotext2,
+        const SizedBox(width: 20.0),
+        radioText
+      ],
+    );
+
+    // スイッチ
+    final switchText = Text(_isOn ? "オン" : "オフ");
+    final toggle = Switch(
+      value: _isOn,
+      onChanged: (value) {
+        setState(() {
+          _isOn = value;
+        });
+      },
+    );
+
+    final switchArea = Row(
+      children: [
+        switchText,
+        toggle,
+      ],
+    );
+
+    final body = SafeArea(
+      child: Column(
+        children: [
+          textArea,
+          buttonArea,
+          textfieldArea,
+          checkboxArea,
+          radioArea,
+          switchArea,
+        ],
+      ),
+    );
 
     return Scaffold(
       body: body,

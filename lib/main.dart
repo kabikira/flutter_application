81845a21e07db82228c6432560bcfa1af1c4454c
kabikira <kabikirar2@protonmail.com>
2024-12-14 03:04:17 +0900

    並び替えリストios機能しない

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,13 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class User {
+  // Userクラス
+  final int id; // ユーザーID
+  final String name; // ユーザー名
+  User(this.id, this.name);
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -29,189 +36,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _msg = "";
-  bool _isChecked = false;
-  int _radioValue = 0;
-  bool _isOn = false;
-  int _counter = 0;
+  final List<User> _users = []; // User型のList
+
+  _MyHomePageState() {
+    for (int i = 1; i <= 50; i++) {
+      // Listに50個分要素を追加。
+      _users.add(User(i, 'ユーザー$i'));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // テキスト
-    const text = Text('テキスト');
-
-    const textArea = Column(
-      children: [text],
-    );
-
-    // ボタン
-    final button = ElevatedButton(
-      onPressed: () {
-        print("ボタンが");
-        print("押されました");
+    final list = ReorderableListView.builder(
+      // 1
+      onReorder: (int oldIndex, int newIndex) {
+        // 2
+        setState(() {
+          // 3
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final User user = _users.removeAt(oldIndex);
+          _users.insert(newIndex, user);
+        });
       },
-      child: const Text("ボタン"),
-    );
-
-    // テキストボタン
-    fn() {
-      print("関数実行");
-    }
-
-    final textbutton = TextButton(onPressed: fn, child: const Text("テキストボタン"));
-
-    final buttonArea = Row(
-      children: [
-        button,
-        textbutton,
-      ],
-    );
-
-// テキストフィールド
-    final textfieldText = Text('メッセージ:$_msg');
-    final controller = TextEditingController();
-
-    final textfield = TextField(
-      controller: controller,
-      autocorrect: true,
-      decoration: const InputDecoration(
-        labelText: "内容",
-        hintText: "100文字以内で入力してください",
+      itemCount: _users.length,
+      itemBuilder: (c, i) => ListTile(
+        key: Key((_users[i].id).toString()), // 4
+        title: Text(_users[i].name),
       ),
-      onChanged: (text) {
-        print("value: $text");
-      },
     );
 
-    setMsg() {
-      setState(() {
-        _msg = controller.text;
-      });
-    }
-
-    clear() {
-      controller.clear();
-    }
-
-    final setMsgButton = ElevatedButton(
-      onPressed: setMsg,
-      child: const Text("反映"),
-    );
-
-    final clearButton = ElevatedButton(
-      onPressed: clear,
-      child: const Text("クリア"),
-    );
-
-    final textfieldArea = Column(
+    final body = Column(
+      // ボディー
       children: [
-        textfieldText,
-        textfield,
-        Row(
-          children: [
-            setMsgButton,
-            clearButton,
-          ],
+        const Text('リストの上'),
+        Expanded(
+          child: list,
         ),
+        const Text('リストの下'),
       ],
-    );
-
-    final isCheckedText = Text("チェック：${_isChecked ? "オン" : "オフ"}");
-
-    final checkbox = Checkbox(
-      value: _isChecked,
-      onChanged: (bool? value) {
-        setState(() {
-          _isChecked = value!;
-        });
-      },
-    );
-
-    const checkboxText = Text("チェックボックス");
-
-    final checkboxArea = Row(
-      children: [
-        isCheckedText,
-        checkbox,
-        checkboxText,
-      ],
-    );
-
-    // ラジオボタン
-    final radio1 = Radio(
-      value: 1,
-      groupValue: _radioValue,
-      onChanged: (value) {
-        setState(() {
-          _radioValue = value!;
-        });
-      },
-    );
-    const radiotext1 = Text('Android');
-
-    final radio2 = Radio(
-      value: 2,
-      groupValue: _radioValue,
-      onChanged: (value) {
-        setState(() {
-          _radioValue = value!;
-        });
-      },
-    );
-    const radiotext2 = Text('iOS');
-
-    const radioMap = {
-      0: "未選択",
-      1: "Android",
-      2: "iOS",
-    };
-    final radioText = Text("デバイス：${radioMap[_radioValue]}");
-
-    final radioArea = Row(
-      children: [
-        radio1,
-        radiotext1,
-        const SizedBox(width: 10.0),
-        radio2,
-        radiotext2,
-        const SizedBox(width: 20.0),
-        radioText
-      ],
-    );
-
-    // スイッチ
-    final switchText = Text(_isOn ? "オン" : "オフ");
-    final toggle = Switch(
-      value: _isOn,
-      onChanged: (value) {
-        setState(() {
-          _isOn = value;
-        });
-      },
-    );
-
-    final switchArea = Row(
-      children: [
-        switchText,
-        toggle,
-      ],
-    );
-
-    final body = SafeArea(
-      child: Column(
-        children: [
-          textArea,
-          buttonArea,
-          textfieldArea,
-          checkboxArea,
-          radioArea,
-          switchArea,
-        ],
-      ),
     );
 
     return Scaffold(
-      body: body,
+      body: body, // ボディー
     );
   }
 }

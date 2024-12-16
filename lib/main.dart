f342faa114902514cd662563fd1fa4bd52856119
kabikira <kabikirar2@protonmail.com>
2024-12-16 13:53:14 +0900

    キーイベント取得

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
-}
+  const text = Text('テキスト');
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  Stream<List<String>> _myWatchEntries() async* {
-    final List<String> datas = [];
-    for (int i = 1; i <= 10; i++) {
-      datas.add("データ:$i");
-      await Future.delayed(const Duration(seconds: 1));
-      yield datas;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final list = StreamBuilder(
-      stream: _myWatchEntries(),
-      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
+  final textFocus = Focus(
+    autofocus: true,
+    onKeyEvent: (node, event) {
+      final key = event.logicalKey;
+      if (event is KeyDownEvent) {
+        if (key == LogicalKeyboardKey.enter) {
+          print('Enterキーが押されました');
+          return KeyEventResult.handled;
         }
+      }
+      return KeyEventResult.ignored;
+    },
+    child: text,
+  );
 
-        final List<String> datas = snapshot.data ?? [];
-
-        return ListView.builder(
-          itemCount: datas.length,
-          itemBuilder: (c, i) => Text(datas[i]),
-        );
-      },
-    );
-    return Scaffold(body: list);
-  }
+  final sc = Scaffold(body: textFocus);
+  final app = MaterialApp(home: sc);
+  runApp(app);
 }

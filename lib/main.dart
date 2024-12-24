b7dffb3f775525dbffe5999d09e8abac8b261eef
kabikira <kabikirar2@protonmail.com>
2024-12-24 21:12:05 +0900

    データ永続化

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // これを追加
-
-class TestIntent extends Intent {} // インテントクラス
-
-void test() async {
-  await Future.delayed(const Duration(seconds: 10));
-  // インテント時の処理関数
-  print('「Control + N」が押された10秒後に実行');
-}
+import 'package:shared_preferences/shared_preferences.dart'; // 1
 
 void main() {
-  // LogicalKeySetオブジェクトを作成（「Control + N」の例）
-  final lksControlN =
-      LogicalKeySet(LogicalKeyboardKey.control, LogicalKeyboardKey.keyN);
+  runApp(const MyApp());
+}
 
-  final text = Text('テキスト');
-  final body = text; // ボディー
-  final faDetector = FocusableActionDetector(
-    // 1
-    autofocus: true, // 2
-    shortcuts: <ShortcutActivator, Intent>{
-      // 3
-      lksControlN: TestIntent(),
-    },
-    actions: {
-      // 4
-      TestIntent: CallbackAction<TestIntent>(
-        onInvoke: (intent) => test(),
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-    },
-    child: body, // 5
-  );
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-  final sc = Scaffold(
-    body: faDetector, // 6
-  );
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
-  final app = MaterialApp(home: sc);
-  runApp(app);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isOn = false;
+
+  _MyHomePageState() {
+    // 8
+    final f = _loadSettingA();
+    f.then((value) => {
+          setState(() {
+            _isOn = value;
+          })
+        });
+  }
+
+  Future<void> _saveSettingA(bool value) async {
+    // 2
+    final prefs = await SharedPreferences.getInstance(); // 3
+
+    await prefs.setBool(
+        // 4
+        'settingA',
+        value);
+  }
+
+  Future<bool> _loadSettingA() async {
+    // 5
+    final prefs = await SharedPreferences.getInstance(); // 6
+    final value = prefs.getBool('settingA');
+    return value ?? false; // 7
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final switchText = Text(_isOn ? "設定A:オン" : "設定A:オフ");
+    final toggle = Switch(
+      value: _isOn,
+      onChanged: (value) {
+        setState(() {
+          _isOn = value;
+          _saveSettingA(value); // 9
+        });
+      },
+    );
+
+    final body = SafeArea(
+      // ボディー
+      child: Row(
+        children: [
+          switchText,
+          toggle,
+        ],
+      ),
+    );
+
+    return Scaffold(
+      body: body, // ボディー
+    );
+  }
 }

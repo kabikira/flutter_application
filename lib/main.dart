015801f596da58d0d307b7818b24590766b830e3
kabikira <kabikirar2@protonmail.com>
2024-12-29 00:21:02 +0900

    アプリバー

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  _mySnackBar(String msg) {
-    return SnackBar(
-      content: Text('【スナックバー】$msg'),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final button = ElevatedButton(
-      onPressed: () {
-        ScaffoldMessenger.of(context).showSnackBar(_mySnackBar("メッセージ"));
-      },
-      child: const Text('ボタン'),
-    );
-    return Scaffold(
-      body: button,
-    );
-  }
+  final appBar = AppBar(
+    title: const Text('アプリのタイトル'),
+  );
+  final sc = Scaffold(
+    appBar: appBar,
+    body: const Text("テキスト"),
+  );
+  final app = MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: sc,
+  );
+  runApp(app);
 }

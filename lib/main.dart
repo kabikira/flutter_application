af75bed71591ca11807c6d0cf769812c945e2e87
kabikira <kabikirar2@protonmail.com>
2024-12-29 00:52:35 +0900

    アプリバー配置追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   final appBar = AppBar(
+    backgroundColor: Colors.orange[50],
     title: const Text('アプリのタイトル'),
+    centerTitle: true,
+    titleTextStyle: const TextStyle(
+      fontStyle: FontStyle.italic,
+      fontSize: 20,
+      color: Colors.green,
+    ),
+    leading: IconButton(onPressed: () {}, icon: const Icon(Icons.info)),
+    actions: [
+      IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+      IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
+    ],
   );
   final sc = Scaffold(
     appBar: appBar,

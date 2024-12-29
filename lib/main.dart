598835c2d0d710f3206c2e37bcaac0d7b5a1907a
kabikira <kabikirar2@protonmail.com>
2024-12-30 00:32:36 +0900

    アプリバードロワー

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  final popupMenu = PopupMenuButton(
-      icon: const Icon(Icons.more_vert),
-      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
-            const PopupMenuItem(
-              value: 'search',
-              child: ListTile(
-                leading: Icon(Icons.search),
-                title: Text('検索する'),
-              ),
-            ),
-            const PopupMenuDivider(),
-            const PopupMenuItem(
-              value: 'calendar',
-              child: ListTile(
-                leading: Icon(Icons.calendar_month),
-                title: Text('カレンダー'),
-              ),
-            ),
-            const PopupMenuItem(
-              value: 'print',
-              child: ListTile(
-                leading: Icon(Icons.print),
-                title: Text('印刷する'),
-              ),
-            )
-          ],
-      onSelected: (value) {
-        switch (value) {
-          case 'search':
-            print("検索");
-          case 'calendar':
-            print("カレンダー");
-          case 'print':
-            print("印刷");
-        }
-      });
+  final dc = DrawerHeader(child: Text('ドロワーのヘッダー'));
+  final drawer = Drawer(
+    child: dc,
+  );
   final appBar = AppBar(
     title: const Text('アプリのタイトル'),
-    actions: [
-      popupMenu,
-    ],
   );
 
   final sc = Scaffold(
     appBar: appBar,
+    drawer: drawer,
     body: const Text("テキスト"),
   );
+
   final app = MaterialApp(
     debugShowCheckedModeBanner: false,
     home: sc,

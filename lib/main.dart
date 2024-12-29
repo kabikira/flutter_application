2340cfbfca05cb3a62df859dd738748420a03fca
kabikira <kabikirar2@protonmail.com>
2024-12-29 10:43:41 +0900

    アプリバーメニューポップアップ追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  final popupMenu = PopupMenuButton(
+      icon: const Icon(Icons.more_vert),
+      itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+            const PopupMenuItem(
+              value: 'search',
+              child: ListTile(
+                leading: Icon(Icons.search),
+                title: Text('検索する'),
+              ),
+            ),
+            const PopupMenuDivider(),
+            const PopupMenuItem(
+              value: 'calendar',
+              child: ListTile(
+                leading: Icon(Icons.calendar_month),
+                title: Text('カレンダー'),
+              ),
+            ),
+            const PopupMenuItem(
+              value: 'print',
+              child: ListTile(
+                leading: Icon(Icons.print),
+                title: Text('印刷する'),
+              ),
+            )
+          ],
+      onSelected: (value) {
+        switch (value) {
+          case 'search':
+            print("検索");
+          case 'calendar':
+            print("カレンダー");
+          case 'print':
+            print("印刷");
+        }
+      });
   final appBar = AppBar(
     title: const Text('アプリのタイトル'),
-    leading: IconButton(
-      onPressed: () {},
-      icon: const Icon(Icons.menu),
-    ),
     actions: [
-      IconButton(
-        onPressed: () {},
-        icon: const Icon(Icons.more_vert),
-      ),
+      popupMenu,
     ],
   );
+
   final sc = Scaffold(
     appBar: appBar,
     body: const Text("テキスト"),

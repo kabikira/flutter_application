2b76d13fb212760b31abd43f5d732015ac135e8b
kabikira <kabikirar2@protonmail.com>
2024-12-30 10:36:51 +0900

    アプリバードロワーリスト

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  final dc = DrawerHeader(child: Text('ドロワーのヘッダー'));
+  final dc = ListView(
+    children: [
+      DrawerHeader(
+        padding: const EdgeInsets.all(0),
+        child: Container(
+          color: Colors.green[50],
+          alignment: Alignment.center,
+          child: const Text('ドロワーのヘッダー'),
+        ),
+      ),
+      ListTile(
+        title: const Text('アイテム1'),
+        onTap: () {
+          print('アイテム1');
+        },
+      ),
+      ListTile(
+        title: const Text('アイテム2'),
+        onTap: () {
+          print('アイテム2');
+        },
+      ),
+      ListTile(
+        title: const Text('アイテム3'),
+        onTap: () {
+          print('アイテム3');
+        },
+      ),
+    ],
+  );
   final drawer = Drawer(
     child: dc,
   );
